542a0f3871426958be172f8cd241018542062cb2
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-07-22 10:01:50 +0200

    Fixed RSpec 2 beta.18 formatter

--- lib/formatters/rspec_two_formatter.rb
+++ lib/formatters/rspec_two_formatter.rb
@@ -5,22 +5,12 @@ require "#{File.dirname(__FILE__)}/../rspactor/notifier"
 class RSpecTwoFormatter < RSpec::Core::Formatters::ProgressFormatter
   include RSpecFormatter
   
-  def dump_summary
-    super
-    message    = rspactor_message(@example_count, failure_count, pending_count, format_seconds(duration))
-    image_path = rspactor_image_path(failure_count, pending_count)
+  def dump_summary(duration, total, failures, pending)
+    super # needed to keep progress formatter
+    message    = rspactor_message(total, failures, pending, duration)
+    image_path = rspactor_image_path(failures, pending)
     
     RSpactor::Notifier.notify(message, rspactor_title, image_path)
-  end
-  
-private
-  
-  def failure_count
-    failed_examples.size
-  end
-  
-  def pending_count
-    pending_examples.size
   end
   
 end
